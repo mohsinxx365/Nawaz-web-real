be3e1cef23db264f54eaa33335c48b2fe4ac64a4
Nawaz Memon <nawazxx333@gmail.com>
2022-01-07 07:42:53 +0300

    a bug fix !

--- public/Login/loginRegister.js
+++ public/Login/loginRegister.js
@@ -44,6 +44,16 @@ function signUp() {
 
       return os;
     }
+    var today = new Date();
+    var date =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate();
+    var time =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date + " " + time;
 
     axios
       .post("https://howto13.loca.lt/register", {
